fd2ed09dbafccc3b31711e46a84a6d20a7e92e8f
winnning godspower <winnninggodspower@gmail.com>
2025-01-07 03:36:44 +0100

    fix: update axios instance to use VITE_API_BASE_URL for base URL configuration

--- React/src/axiosInstance.js
+++ React/src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000';
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
 
 const axiosInstance = axios.create({
   baseURL,
