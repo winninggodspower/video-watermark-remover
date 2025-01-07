9899b4183d7187b36a07bc072f7d7b3b0b5a7a0b
winnning godspower <winnninggodspower@gmail.com>
2025-01-07 10:46:04 +0100

    fix: remove commented-out code and unnecessary headers in axios instance

--- React/src/axiosInstance.js
+++ React/src/axiosInstance.js
@@ -4,9 +4,6 @@ const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
 
 const axiosInstance = axios.create({
   baseURL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 export default axiosInstance;
